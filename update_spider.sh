f10c1841b01a962fb6a63c6e113d761a47f316bc
wenhui007 <44024509+wenhui007@users.noreply.github.com>
2023-01-30 12:02:42 +0800

    Update update_spider.sh
    del log

--- update_spider.sh
+++ update_spider.sh
@@ -20,14 +20,12 @@ for str in $containerId
 do
   docker rmi $str
 done
-docker images
 
 echo "FROM python:3.9.13
 RUN apt-get update && apt-get install vim -y && apt-get install git -y
 WORKDIR /work
 RUN git clone --depth 1 https://code_commiter-at-358135713291:yjPPnVzmPyGWOW5HvkIycyELrwc2XN8cghHwLFFOHCY=@git-codecommit.us-west-1.amazonaws.com/v1/repos/twitter_spider
 WORKDIR /work/twitter_spider/
-RUN ls
 RUN pip install -r  requirements.txt
 CMD  python /work/twitter_spider/run.py --start && /bin/bash" > Dockerfile
 echo "构建镜像"
